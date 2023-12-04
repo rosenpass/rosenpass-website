a0ccc7ced46babbb04c0a141688d8f8110c25851
Mullana <Mullana@users.noreply.github.com>
2023-12-04 13:43:02 +0000

    deploy: be247663ec6ce805b7c7ef42ad6be6bf40b24454

---
+++ presentations/slides/flake.nix
@@ -0,0 +1,103 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    devshell.url = "github:numtide/devshell";
+    devshell.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, devshell, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs =
+          import nixpkgs {
+            inherit system;
+            overlays = [ devshell.overlays.default ];
+          };
+        tex = pkgs.texlive.combine {
+          inherit (pkgs.texlive)
+            scheme-basic amsfonts beamer babel-german tcolorbox emoji environ
+            ccicons csquotes csvsimple doclicense fancyvrb fontspec gobble
+            koma-script ifmtarg latexmk lm markdown mathtools minted noto
+            nunito pgf relsize soul unicode-math lualatex-math gitinfo2
+            eso-pic biblatex biblatex-trad biblatex-software xkeyval xurl
+            xifthen biber;
+        };
+
+        documents = (builtins.fromTOML (builtins.readFile ./documents.toml)).documents;
+      in
+      {
+        packages = builtins.listToAttrs (
+          builtins.map
+            ({ name, ... } @ meta:
+              {
+                inherit name;
+                value = pkgs.stdenvNoCC.mkDerivation {
+                  inherit name;
+                  srcs = [ (./. + "/${name}") ./tex ];
+                  sourceRoot = "./${name}";
+                  nativeBuildInputs = with pkgs; [
+                    tex
+                  ];
+                  buildPhase = ''
+                    export HOME=$(mktemp -d)
+                    latexmk
+                  '';
+                  installPhase = ''
+                    mkdir --parent -- $out
+                    mv *.pdf $out/
+                  '';
+                  passthru = meta;
+                };
+              }
+            )
+            documents);
+
+        devShells.default = (pkgs.devshell.mkShell {
+          imports = [ "${devshell}/extra/git/hooks.nix" ];
+          name = "rosenpass-slides-dev-shell";
+          packages = with pkgs; [ tex nixpkgs-fmt nodePackages.prettier ];
+          git.hooks = {
+            enable = true;
+            pre-commit.text = ''
+              nix flake check
+            '';
+          };
+          commands = [
+            {
+              name = "build";
+              command = ''
+                latexmk
+              '';
+              help = "build the slides";
+            }
+            {
+              name = "clean";
+              command = ''
+                latexmk -c
+              '';
+              help = "build the slides";
+            }
+          ];
+        });
+
+        checks = {
+          nixpkgs-fmt = pkgs.runCommand "check-nixpkgs-fmt"
+            { nativeBuildInputs = [ pkgs.nixpkgs-fmt ]; } ''
+            nixpkgs-fmt --check ${./.} && touch $out
+          '';
+          prettier-check = pkgs.runCommand "check-with-prettier"
+            { nativeBuildInputs = [ pkgs.nodePackages.prettier ]; } ''
+            cd ${./.} && prettier --check . && touch $out
+          '';
+
+          # 1. for every folder that is not hidden there must be a package
+          # consistency-check = pkgs.runCommand "check-consistency"
+          #   { nativeBuildInputs = [ ]; } ''
+          #   cd ${./.}
+          #   find -mindepth 1 -maxdepth 1 -type d -not -path '*/.*'
+          # '';
+        };
+      }
+    );
+}
